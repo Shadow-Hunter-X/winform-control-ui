8e02a630a19e623c7fa3a680ba1d84f583c9d221
J20 <LYJ__h@163.com>
2020-09-18 17:18:20 +0800

    update

---
+++ code/ctlClockLib/Test/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace Test
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ctlAlarmClock1 = new ctlClockLib.ctlAlarmClock();
+            this.dtpTest = new System.Windows.Forms.DateTimePicker();
+            this.lblTest = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // ctlAlarmClock1
+            // 
+            this.ctlAlarmClock1.AlarmSet = false;
+            this.ctlAlarmClock1.AlarmTime = new System.DateTime(((long)(0)));
+            this.ctlAlarmClock1.ClockBackColor = System.Drawing.Color.Empty;
+            this.ctlAlarmClock1.ClockForeColor = System.Drawing.Color.Empty;
+            this.ctlAlarmClock1.Location = new System.Drawing.Point(12, 54);
+            this.ctlAlarmClock1.Name = "ctlAlarmClock1";
+            this.ctlAlarmClock1.Size = new System.Drawing.Size(135, 96);
+            this.ctlAlarmClock1.TabIndex = 0;
+            // 
+            // dtpTest
+            // 
+            this.dtpTest.Format = System.Windows.Forms.DateTimePickerFormat.Time;
+            this.dtpTest.Location = new System.Drawing.Point(12, 156);
+            this.dtpTest.Name = "dtpTest";
+            this.dtpTest.Size = new System.Drawing.Size(200, 21);
+            this.dtpTest.TabIndex = 1;
+            this.dtpTest.ValueChanged += new System.EventHandler(this.dtpTest_ValueChanged);
+            // 
+            // lblTest
+            // 
+            this.lblTest.AutoSize = true;
+            this.lblTest.Location = new System.Drawing.Point(30, 18);
+            this.lblTest.Name = "lblTest";
+            this.lblTest.Size = new System.Drawing.Size(35, 12);
+            this.lblTest.TabIndex = 2;
+            this.lblTest.Text = "     ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.lblTest);
+            this.Controls.Add(this.dtpTest);
+            this.Controls.Add(this.ctlAlarmClock1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private ctlClockLib.ctlAlarmClock ctlAlarmClock1;
+        private System.Windows.Forms.DateTimePicker dtpTest;
+        private System.Windows.Forms.Label lblTest;
+    }
+}
+
